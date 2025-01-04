446368dac1b81e587538d3c42868c483a16dd053
Harry <harsh.225267101@vcet.edu.in>
2025-01-04 23:57:25 +0530

    added speed insights

--- aiJsCompiler/src/App.jsx
+++ aiJsCompiler/src/App.jsx
@@ -1,9 +1,11 @@
 import JsCompiler from "./Compiler/JsCompiler"
+import { SpeedInsights } from '@vercel/speed-insights/react';
 function App() {
 
   return (
     <>
       <JsCompiler/>
+      <SpeedInsights/>
       </>
   )
 }
