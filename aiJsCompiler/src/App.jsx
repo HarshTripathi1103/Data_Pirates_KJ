e4599ad3c76d4571f2d70a21105a8eceaca65caf
Harry <harsh.225267101@vcet.edu.in>
2025-01-05 00:08:20 +0530

    added analytics

--- aiJsCompiler/src/App.jsx
+++ aiJsCompiler/src/App.jsx
@@ -1,11 +1,13 @@
 import JsCompiler from "./Compiler/JsCompiler"
 import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react';
 function App() {
 
   return (
     <>
       <JsCompiler/>
       <SpeedInsights/>
+      <Analytics/>
       </>
   )
 }
